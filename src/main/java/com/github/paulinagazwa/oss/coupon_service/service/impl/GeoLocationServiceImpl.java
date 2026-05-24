39fea0371ca6dc4451c0cd6cdab86901b9812da0
Paulina Gązwa <paulina.gazwa@gmail.com>
2026-05-24 22:48:22 +0200

    ISSUE-3 added empty GeoLocationService

---
+++ src/main/java/com/github/paulinagazwa/oss/coupon_service/service/impl/GeoLocationServiceImpl.java
@@ -0,0 +1,14 @@
+package com.github.paulinagazwa.oss.coupon_service.service.impl;
+
+import com.github.paulinagazwa.oss.coupon_service.service.GeoLocationService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GeoLocationServiceImpl implements GeoLocationService {
+
+	@Override
+	public String resolveCountry(String ipAddress) {
+
+		return "";
+	}
+}
